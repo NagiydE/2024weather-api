46097bd35e053199b922b68f01cf3cb59c5e5dd0
Nagiyd <najj93@gmail.com>
2024-10-21 12:17:51 -0400

    git commit m- working weather API project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+/* the specific codes that I would like to use are :
+city_name: City name.
+description: Text weather description.
 
+*/
+
+document.querySelector('#realbutton').addEventListener('click', getTemperature)
+const apiKey = '0d50e678a6cf4bfc94d0b17d3a7d1f89'
+function getTemperature() {
+    const city = document.querySelector('#city').value
+    const country = document.querySelector('#country').value
+
+    const url = `https://api.weatherbit.io/v2.0/current?key=${apiKey}&city=${city}&country=${country}&units=I`
+
+    console.log('This is the entered city:', city)
+    console.log('This is the entered country:', country)
+
+    fetch(url)
+
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            console.log(data.data[0].city_name)
+            console.log(data.data[0].state_code)
+            console.log(data.data[0].temp)
+            console.log(data.data[0].country_code)
+
+
+
+
+            document.querySelector('h2').textContent = `The Current temperature in ${city} is ${data.data[0].temp} degrees fahrenheit.`
+
+
+        })
+        .catch(err => {
+            console.error(`error ${err}`)
+        })
+
+}
